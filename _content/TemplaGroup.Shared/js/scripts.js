b5c1e5dafba8a1386150852f44c145418aaa6fe4
templa-group <>
2024-08-12 17:13:55 +0000

    Update from https://github.com/templa-group/webapp/commit/a34fc7f0f8e164bd4509585f03fc3cc8c37864c9

--- _content/TemplaGroup.Shared/js/scripts.js
+++ _content/TemplaGroup.Shared/js/scripts.js
@@ -5,3 +5,23 @@ window.removeQueryParam = function (param) {
     url.searchParams.delete(param);
     window.history.replaceState({}, '', url);
 };
+
+window.generateKeyPair = async function () {
+    const keyPair = await crypto.subtle.generateKey(
+        {
+            name: "RSASSA-PKCS1-v1_5",
+            modulusLength: 2048,
+            publicExponent: new Uint8Array([1, 0, 1]),
+            hash: { name: "SHA-256" },
+        },
+        true,
+        ["sign", "verify"]
+    );
+    let publicKey = await crypto.subtle.exportKey("spki", keyPair.publicKey);
+    let privateKey = await crypto.subtle.exportKey("pkcs8", keyPair.privateKey);
+    
+    return JSON.stringify({
+        publicKey: btoa(String.fromCharCode(...new Uint8Array(publicKey))),
+        privateKey: btoa(String.fromCharCode(...new Uint8Array(privateKey)))
+    });
+}
