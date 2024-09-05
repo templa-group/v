1b24a035b266c300117dbe9e841ccfac1d6b1330
templa-group <>
2024-09-05 01:37:42 +0000

    Update from https://github.com/templa-group/webapp/commit/4066ed65be906ead230575a5dddd7e1bfc68fb79

--- js/scripts.js
+++ js/scripts.js
@@ -11,26 +11,10 @@ class Helpers {
 
 window.Helpers = Helpers;
 
-// Canvas picture
-function toBase64(id) {
-    let canvas = document.querySelectorAll("[".concat("_bl_").concat(id).concat('=""]'));
-    return canvas[0].toDataURL('image/png').replace(/^data:image\/png;base64,/, '');
-}
-
 // Culture
 window.cultureInfo = {
     get: () => window.localStorage['AppCulture'],
     set: (value) => window.localStorage['AppCulture'] = value
-};
-
-// Save file
-window.saveAsFile = (fileName, content) => {
-    const blob = new Blob([content], {type: 'application/pdf'});
-    const link = document.createElement('a');
-    link.href = URL.createObjectURL(blob);
-    link.download = fileName;
-    link.click();
-    URL.revokeObjectURL(link.href);
 };
 
 // Barcode
